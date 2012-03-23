8a59d09ac04bb5771942bea934b1534f33eadbba
Cody Fails <codyfails@gmail.com>
2012-03-22 19:06:58 -0500

    current code

---
+++ CombatReader/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CombatReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TextWriter tw = new TextWriter()
+            using (TextReader tr = new StreamReader("ComLog1.txt"))
+            {
+                string preEventTime = tr.ReadLine().Split(']')[0];
+                DateTime eventTime = Convert.ToDateTime(preEventTime.Remove(0, 1));
+                Console.WriteLine(preEventTime);
+                Console.WriteLine(eventTime);
+                string preSource = tr.ReadLine().Split(']')[1];
+                Console.WriteLine(preSource.Remove(0, 2));
+                Console.ReadLine();
+            }
+        }
+    }
+}
