1526ec004b3f47e5aff174d8d91ca6346f5915bb
Mark Rayburn <mrayburn@gmail.com>
2012-03-24 10:11:54 -0500

    classwork

--- CombatReader/EventLine.cs
+++ CombatReader/EventLine.cs
@@ -19,6 +19,12 @@ namespace CombatReader
         public AbilityName AbilityName { get; set; }
         public string Event { get; set; }
         public string Effect { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Source { get; set; }
+        public string Target { get; set; }
+        public string AbilityName { get; set; }
+        public string Event { get; set; }
+        public string Effect { get; set; }
         public int Value { get; set; }
         public Mitigation Mitigation { get; set; }
         public int Threat { get; set; }
--- CombatReader/Program.cs
+++ CombatReader/Program.cs
@@ -12,7 +12,8 @@ namespace CombatReader
         {
             var list = new List<EventLine>();
             //TextWriter tw = new TextWriter()
-            using (TextReader tr = new StreamReader(@"C:\source\CombatReader\ComLog1.txt"))
+
+            using (TextReader tr = new StreamReader("..\\..\\ComLog1.txt"))
             {
                 //int counter = 0;
                 bool isEOF = false;
@@ -76,6 +77,41 @@ namespace CombatReader
                     list.Add(el);
                 } while (isEOF == false);
                 //Console.WriteLine(list.Count);
+                int counter = 0;
+                do
+                {
+                    counter++;
+                    EventLine el = new EventLine();
+                    string firstLine = tr.ReadLine();
+                    el.TimeStamp = Convert.ToDateTime(firstLine.Split(']')[0].Remove(0, 1));
+                    Console.WriteLine(el.TimeStamp);
+                    el.Source = firstLine.Split(']')[1].Remove(0, 2);
+                    Console.WriteLine(el.Source);
+                    el.Target = firstLine.Split(']')[2].Remove(0, 2);
+                    Console.WriteLine(el.Target);
+                    el.AbilityName = firstLine.Split(']')[3].Remove(0, 2);
+                    Console.WriteLine(el.AbilityName);
+                    string ee = firstLine.Split(']')[4].Remove(0, 2);
+                    el.Event = ee.Split(':')[0];
+                    Console.WriteLine(el.Event);
+                    el.Effect = ee.Split(':')[1].Remove(0, 1);
+                    Console.WriteLine(el.Effect);
+                    string valSplit = firstLine.Split('(')[1];
+                    string preVal = valSplit.Split(')')[0];
+                    int eVal;
+                    bool isAnumber = int.TryParse(preVal, out eVal);
+                    el.Value = eVal;
+                    Console.WriteLine(el.Value);
+                    bool isThreat = valSplit.Contains('<');
+                    if (isThreat)
+                    {
+                        string preThreat = valSplit.Split('<')[1].Replace(">", "");
+                        int eThreat;
+                        bool isAnumber2 = int.TryParse(preThreat, out eThreat);
+                        el.Threat = eThreat;
+                        Console.WriteLine(el.Threat);
+                    }
+                } while (counter < 30);
                 Console.ReadLine();
             }
         }
