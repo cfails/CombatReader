077738bfecbb2d47ec159fba84769e1949660b2a
Cody Fails <codyfails@gmail.com>
2012-03-24 12:22:14 -0500

    CombatReader

--- CombatReader/Program.cs
+++ CombatReader/Program.cs
@@ -10,45 +10,64 @@ namespace CombatReader
     {
         static void Main(string[] args)
         {
+            var list = new List<EventLine>();
             //TextWriter tw = new TextWriter()
-            using (TextReader tr = new StreamReader(@"C:\source\CombatReader\ComLog1.txt"))
+            using (TextReader tr = new StreamReader(@"C:\source\CombatReader\ComLogTest.txt"))
             {
-                int counter = 0;
+                //int counter = 0;
+                bool isEOF = false;
                 do
                 {
-                    counter++;
+                    //counter++;
                     EventLine el = new EventLine();
                     string firstLine = tr.ReadLine();
-                    el.TimeStamp = Convert.ToDateTime(firstLine.Split(']')[0].Remove(0, 1));
-                    Console.WriteLine(el.TimeStamp);
-                    el.Source = firstLine.Split(']')[1].Remove(0, 2);
-                    Console.WriteLine(el.Source);
-                    el.Target = firstLine.Split(']')[2].Remove(0, 2);
-                    Console.WriteLine(el.Target);
-                    el.AbilityName = firstLine.Split(']')[3].Remove(0, 2);
-                    Console.WriteLine(el.AbilityName);
-                    string ee = firstLine.Split(']')[4].Remove(0, 2);
-                    el.Event = ee.Split(':')[0];
-                    Console.WriteLine(el.Event);
-                    el.Effect = ee.Split(':')[1].Remove(0, 1);
-                    Console.WriteLine(el.Effect);
-                    string valSplit = firstLine.Split('(')[1];
-                    string preVal = valSplit.Split(')')[0];
-                    int intVal;
-                    bool isANumber = int.TryParse(preVal, out intVal);
-                    el.Value = intVal;
-                    Console.WriteLine(el.Value);
-                    bool isThreat = valSplit.Contains('<');
-                    if (isThreat)
+                    if (firstLine != null)
                     {
-                        string preThreat = valSplit.Split('<')[1].Replace(">", "");
-                        int threat;
-                        bool isANumber2 = int.TryParse(preThreat, out threat);
-                        el.Threat = threat;
+                        el.TimeStamp = Convert.ToDateTime(firstLine.Split(']')[0].Remove(0, 1));
+                        Console.WriteLine(el.TimeStamp);
+                        el.Source = firstLine.Split(']')[1].Remove(0, 2);
+                        Console.WriteLine(el.Source);
+                        el.Target = firstLine.Split(']')[2].Remove(0, 2);
+                        Console.WriteLine(el.Target);
+                        el.AbilityName = firstLine.Split(']')[3].Remove(0, 2);
+                        Console.WriteLine(el.AbilityName);
+                        string ee = firstLine.Split(']')[4].Remove(0, 2);
+                        el.Event = ee.Split(':')[0];
+                        Console.WriteLine(el.Event);
+                        el.Effect = ee.Split(':')[1].Remove(0, 1);
+                        Console.WriteLine(el.Effect);
+                        string valSplit = firstLine.Split('(')[1];
+                        int index = valSplit.IndexOf(' ');
+                        string preVal; 
+                        if (index != -1)
+                        {
+                            preVal = valSplit.Remove(index).Replace(")", "").Replace("*", "");
+                        }
+                        else
+                        {
+                            preVal = valSplit.Split(')')[0].Replace("*", "");
+                        }
+                        int intVal;
+                        bool isNumber = int.TryParse(preVal, out intVal);
+                        el.Value = intVal;
+                        Console.WriteLine(el.Value);
+                        bool isThreat = valSplit.Contains('<');
+                        if (isThreat)
+                        {
+                            string preThreat = valSplit.Split('<')[1].Replace(">", "");
+                            int threat;
+                            bool isNumber2 = int.TryParse(preThreat, out threat);
+                            el.Threat = threat;
+                            Console.WriteLine(el.Threat);
+                        }
                     }
-                    Console.WriteLine(el.Threat);	
-                } while (counter < 30);
-                
+                    else
+                    {
+                        isEOF = true;
+                    }
+                    list.Add(el);
+                } while (isEOF == false);
+                //Console.WriteLine(list.Count);
                 Console.ReadLine();
             }
         }
